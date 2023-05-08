d03fbbf5af7ae6de8eb9b6e6b4aabb590f171414
Lelena1 <luppol@inbox.ru>
2023-05-08 15:57:20 +0300

    Operation in Repository

--- src/main/java/me/luppolem/firstbot/repository/NotificationTaskRepository.java
+++ src/main/java/me/luppolem/firstbot/repository/NotificationTaskRepository.java
@@ -3,5 +3,11 @@ package me.luppolem.firstbot.repository;
 import me.luppolem.firstbot.entity.NotificationTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask,Long> {
+
+    List<NotificationTask> findAllByNotificationDateTime(LocalDateTime localDateTime);
+    List<NotificationTask>findAllByNotificationDateTimeAndChatId(LocalDateTime localDateTime,long chatId);
 }
